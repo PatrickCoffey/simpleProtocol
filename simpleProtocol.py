8ab6b3bf68d64b44e8cd0aedb0d7340213e3dba8
schlerp <patrick.coffey@live.com.au>
2015-01-17 12:18:19 +0930

    tidy up of base object

--- simpleProtocol.py
+++ simpleProtocol.py
@@ -1,70 +1,33 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*
 
-from serial import Serial
 import time
 import binascii
 
-class serialComBase(Serial):
+class comBase(object):
     """
-    Base Serial Communication Object:
-        This represents a device connected via serial to the host. 
-        This class houses all the code used internally to the function.
+    Base Communication Object:
+        This represnts a protocol object base. it can be used to
+        send and recieve data to/from another device.
         Other classes can inherit from this and add user functions.
         
-        communicates using packets, an example:
-            
-        
-        Check pySerial Documentation
+        communicates using packets with `message|crc`, an example:
+            the string: 
+                'derp a herp'
+            wraps to:
+                '`derp a herp|-12345`' 
+            (where -12345 is the CRC)
     """
     
     HEADER_CHAR = '`'
     DELIM_CHAR = '|'
     ESCAPE_CHAR = '\\'
     
-    STATUS_AWAKE = '0 - Awake!'
-    STATUS_BUSY = '1 - Busy!'
-    CHAR_READ = 'R'
-    CHAR_STATUS = 'S'
-    CHAR_SLEEP = 'P'
-    
-    #def __init__(self, comPort='/dev/ttyACM1', baudRate=9600):
-        #"""Overloaded to set default values - Check pySerial Documentation"""
-        #Serial.__init__(self, comPort, baudRate)
-        #print("Initialised Serial connection")
-        #time.sleep(3)
-    
-    def _readChar(self):
-        """Read a single byte from the Serial buffer"""
-        ret = ''
-        if self.isOpen():
-            ret = self.read(1)
-            return(ret)
-        
-    #def _readBuff(self):
-        #"""Read the Serial buffer and return each char one at a time like a generator"""
-        #ret = ''
-        #if self.isOpen():
-            #while self.inWaiting > 0:
-                #ret = self.read(1)
-                #yield ret    
-
-    def _readTest(self, sMessage):
-        """Reads the test String back one character at a time"""
+    def _yieldMessage(self, sMessage):
+        """Reads the String back one character at a time"""
         for char in sMessage:
             yield char
-                
-    def _hasChars(self):
-        """Checks if there are characters in the serial buffer"""
-        ret = ''
-        if self.isOpen():
-            while self.inWaiting > 0:
-                return True
-            else:
-                return False
-        else:
-            return 2
-    
+                    
     def _wrapPacket(self, sMessage):
         """Wraps the data/command into a packet to transmit"""
         temp = sMessage.replace('`', '\\`')
@@ -83,7 +46,7 @@ class serialComBase(Serial):
         ret = ''
         crc = ''
         
-        for char in self._readTest(sMessage):
+        for char in self._yieldMessage(sMessage):
             if inFooter == True and inMessage == True:
                 print('Error: inmessage and inheader')
             if inEscape == False:
@@ -117,7 +80,7 @@ class serialComBase(Serial):
             return 2
         
     def _calcCRC(self, sMessage):
-        # calculate and return crc
+        """Calculate and return CRC32 (int) of sMessage"""
         return binascii.crc32(sMessage)
     
     def _checkCRC(self, sMessage, CRC):
@@ -125,43 +88,12 @@ class serialComBase(Serial):
             return True
         else:
             return False
-    
-    #def _getStream(self):
-        #"""Gets the Stream of data form the device using self._readBuff"""
-        #inMessage = False
-        #inEscape = False
-        #ret = ''
-        #while self._hasChars():
-            #for char in self._readBuff():
-                #if inEscape == False:
-                    #if char == self.HEADER_CHAR:
-                        #if inMessage == False:
-                            #inMessage = True 
-                    #elif char == self.ESCAPE_CHAR:
-                        #inEscape = True
-                    #else:
-                        #ret += char
-                #elif inEscape == True:
-                    #if inMessage == True:
-                        #ret += char
-        #if ret == '':
-            #return 2
-        #else:
-            #return ret
             
-    def _getTestStream(self, sMessage):
-        """Gets the Stream of data form the device using self._readBuff"""
-        ret = self._unwrapPacket(sMessage)
-                        
-        if ret == '':
-            return 2
-        else:
-            return ret
-        
 if __name__ == '__main__':
-    test = serialComBase()
+    test = comBase()
     initialMessage = 'i wonder how long these messages can be? and what sorts of characters they can contain? <>:"{}+_)_'
+    print(initialMessage)
     sMessage = test._wrapPacket(initialMessage)
     print(sMessage)
-    ret = test._getTestStream(sMessage)
-    print(ret)
+    ret = test._unwrapPacket(sMessage)
+    print(ret + '\n')
